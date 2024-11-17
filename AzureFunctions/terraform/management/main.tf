9f30fd3ff0f8ebc338ad5257aeaac50d5f495db1
WiseOldTurtle <67073808+WiseOldTurtle@users.noreply.github.com>
2024-11-17 04:13:03 +0000

    Updated files

--- AzureFunctions/terraform/management/main.tf
+++ AzureFunctions/terraform/management/main.tf
@@ -19,19 +19,28 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-# Create Resource Group
-resource "azurerm_resource_group" "webapp_rg" {
-  name     = var.resource_group_name
-  location = var.location
+
+resource "azurerm_resource_group" "imagesRG" {
+  name     = "rg-imageplatform-wotlab01"
+  location = "West Europe"
 }
 
-# Create Storage Account for webapp backend
-resource "azurerm_storage_account" "webappstore" {
-  name                      = var.storage_account_name
-  resource_group_name       = azurerm_resource_group.webapp_rg.name
-  location                  = var.location
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
+resource "random_id" "unique_id" {
+  byte_length = 8
+}
+
+resource "azurerm_storage_account" "image_storage" {
+  name                     = "imagestorageaccount${random_id.unique_id.hex}"
+  resource_group_name      = azurerm_resource_group.imagesRG.name
+  location                 = azurerm_resource_group.imagesRG.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "images" {
+  name                  = "images"
+  storage_account_id    = azurerm_storage_account.image_storage.id
+  container_access_type = "blob"
 }
 
 # Create Key Vault # TODO
@@ -77,6 +86,6 @@ output "github_token_secret_id" {
 
 # Output the Storage Account Connection String
 output "storage_account_connection_string" {
-  value = azurerm_storage_account.webappstore.primary_connection_string
+  value = azurerm_storage_account.image_storage.primary_connection_string
   sensitive = true
 }
--- AzureFunctions/terraform/webapp/main.tf
+++ AzureFunctions/terraform/webapp/main.tf
@@ -42,69 +42,68 @@ resource "azurerm_static_site" "frontend" {
   sku_tier            = "Free"  # For cost-effective solution
 }
 
-# ARM Template deployment reference (terraform module workaround) # TODO. Check for other options
+# ARM Template deployment reference (terraform module workaround)
 resource "azurerm_resource_group_template_deployment" "frontend_appsettings" {
   name                = "frontend-webapp-casestudy"
   resource_group_name = azurerm_resource_group.webapp_rg.name
   deployment_mode     = "Incremental"
 
-  # Reference the ARM template file 
+  # Reference the ARM template file
   template_content = file("${path.module}/staticwebapp-staticsite.json")
 
   parameters_content = jsonencode({
     staticSiteName          = { value = azurerm_static_site.frontend.name }
     storageConnectionString = { value = data.terraform_remote_state.management.outputs.storage_account_connection_string }  # Pull from management state file
     imageResolution         = { value = var.image_resolution }
-    # linkShortenerApiKey     = { value = var.link_shortener_api_key } # TODO.
-    # authSecret              = { value = var.auth_secret } # TODO.
     logLevel                = { value = var.log_level }
   })
 }
 
 # Function App Plan (Consumption Plan)
 resource "azurerm_function_app_plan" "function_plan" {
-  name                     = "function-app-plan"
-  location                 = azurerm_resource_group.webapp_rg.location
-  resource_group_name      = azurerm_resource_group.webapp_rg.name
-  kind                     = "FunctionApp"
+  name                = "function-app-plan"
+  location            = azurerm_resource_group.webapp_rg.location
+  resource_group_name = azurerm_resource_group.webapp_rg.name
+  kind                = "FunctionApp"
   sku {
     tier = "Dynamic"  # Consumption Plan
     size = "Y1"       # For cost-effective hosting
   }
 }
 
-# Function App Resource
-resource "azurerm_function_app" "function_app" {
+# Linux Function App Resource
+resource "azurerm_linux_function_app" "function_app" {
   name                      = "frontend-function-app"
   location                  = azurerm_resource_group.webapp_rg.location
   resource_group_name       = azurerm_resource_group.webapp_rg.name
-  app_service_plan_id       = azurerm_function_app_plan.function_plan.id
-  storage_account_name     = azurerm_storage_account.webappstore.name
-  storage_account_access_key = azurerm_storage_account.webappstore.primary_access_key
-  os_type                   = "Linux"
-  version                   = "~3"
+  service_plan_id           = azurerm_function_app_plan.function_plan.id
+  storage_account_name      = data.terraform_remote_state.management.outputs.storage_account_name
+  storage_account_access_key = data.terraform_remote_state.management.outputs.storage_account_access_key
+  
 
   site_config {
-    linux_fx_version = "NODE|14"  
+    application_stack {
+      node_version = "14"
+    }
   }
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME" = "node"
-    "AzureWebJobsStorage" = azurerm_storage_account.webappstore.primary_connection_string
-    "GITHUB_TOKEN" = data.azurerm_key_vault_secret.github_token.value  # Referencing the GitHub token from Key Vault
+    "AzureWebJobsStorage"      = data.terraform_remote_state.management.outputs.storage_account_connection_string
+    "GITHUB_TOKEN"             = data.azurerm_key_vault_secret.github_token.value
   }
 }
 
 # Data source for GitHub token from Key Vault
 data "azurerm_key_vault_secret" "github_token" {
   name         = "github-token"
-  key_vault_id = azurerm_key_vault.kv-wotlab01.id 
+  key_vault_id = data.terraform_remote_state.management.outputs.key_vault_id
 }
 
 # Link GitHub Repository to Function App
-resource "azurerm_function_app_source_control" "github" {
-  function_app_id  = azurerm_function_app.function_app.id
+resource "azurerm_linux_function_app_source_control" "github" {
+  function_app_id  = azurerm_linux_function_app.function_app.id
   repo_url         = "https://github.com/WiseOldTurtle/ImageSharingPlatform"
-  branch           = "main" 
-  repo_token       = data.azurerm_key_vault_secret.github_token.value  # Use the GitHub token from Key Vault
+  branch           = "main"
+  repo_token       = data.azurerm_key_vault_secret.github_token.value
 }
