a3056bbfd0047a1914519d38fa3e271d9a41fc21
WiseOldTurtle <67073808+WiseOldTurtle@users.noreply.github.com>
2024-11-17 08:04:05 +0000

    added in keyvault output

--- AzureFunctions/terraform/management/main.tf
+++ AzureFunctions/terraform/management/main.tf
@@ -85,10 +85,9 @@ output "github_token_secret_id" {
   value = azurerm_key_vault_secret.github_token.id
 }
 
-
-# output "key_vault_id" {
-#   value = azurerm_key_vault.kv-wotlab01.id 
-# }
+output "key_vault_id" {
+  value = azurerm_key_vault.kv-wotlab01.id 
+}
 
 
 # Output the Storage Account Connection String
