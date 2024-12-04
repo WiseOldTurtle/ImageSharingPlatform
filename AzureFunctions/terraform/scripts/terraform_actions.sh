70d223fbb0367ecbd7d6c19c40e927adb446fc11
WiseOldTurtle <67073808+WiseOldTurtle@users.noreply.github.com>
2024-12-04 12:14:12 +0000

    added some changes

--- AzureFunctions/terraform/scripts/terraform_actions.sh
+++ AzureFunctions/terraform/scripts/terraform_actions.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# terraform_actions.sh
 
 set -e
 
@@ -9,28 +8,19 @@ WORKING_DIR=$3
 
 echo "Running Terraform $ACTION for $DIR"
 
-# Ensure the GitHub token is set
-if [[ -z "$GITHUB_ACCESS_TOKEN" ]]; then
-  echo "Error: GITHUB_ACCESS_TOKEN is not set!"
-  exit 1
-fi
-
 # Initialize Terraform
 terraform -chdir="$WORKING_DIR/$DIR" init \
   -backend-config="resource_group_name=$backendRGName" \
   -backend-config="storage_account_name=$backendStorageAccountName" \
   -backend-config="container_name=$backendContainerName" \
-  -backend-config="key=${DIR}.tfstate" \
-  -backend-config="access_key=$ARM_ACCESS_KEY" \
-  -var "github_access_token=$GITHUB_ACCESS_TOKEN"
-  
+  -backend-config="key=${DIR}.tfstate"
 
 # Perform action-specific tasks
 case "$ACTION" in
   Test)
     mkdir -p "$WORKING_DIR/validation-reports/$DIR"
     terraform -chdir="$WORKING_DIR/$DIR" validate > "$WORKING_DIR/validation-reports/$DIR/validate.log"
-    terraform -chdir="$WORKING_DIR/$DIR" fmt -check >> "$WORKING_DIR/validation-reports/$DIR/fmt.log"
+    terraform -chdir="$WORKING_DIR/$DIR" fmt -check > "$WORKING_DIR/validation-reports/$DIR/fmt.log"
     ;;
   Plan)
     mkdir -p "$WORKING_DIR/plan-reports/$DIR"
